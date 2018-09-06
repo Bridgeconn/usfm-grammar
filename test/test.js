1713ed8d561f1cd006f5ffaa725d87b11c524f3f
kavitharaju <kavitharaju18@gmail.com>
2018-09-06 17:39:45 +0530

    Added sample test cases

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
 var parser = require("../parser.js")
+var fs = require('fs')
 
 
 describe('Grammar', function() {
@@ -18,5 +19,131 @@ describe('Grammar', function() {
       var output = parser.validator("\\id")
       assert.equal(output, false);
     });
+  });
+
+
+  describe("#mandatory_markers",function(){
+	it('the 4 mandatory_markers are id,c,p,v ',function(){
+  		fs.readFile('test/test_simple_ID_C_P_V_correct.usfm', function(err, data) {
+			if(err) throw err;
+
+	  		var input = data;
+	  		var output = parser.validator(input);
+	  		assert.equal(output,true);
+	  	});
+  	}); 	
+
+  	it('Should start with ID',function(){
+  		fs.readFile('test/test_simple_IDnotStarting_wrong.usfm', function(err, data) {
+			if(err) throw err;
+
+	  		var input = data;
+	  		var output = parser.validator(input);
+	  		assert.equal(output,false);
+	  	});
+  	});
+
+	it('ID is a mandatory_marker',function(){
+  		fs.readFile('test/test_simple_noID_wrong.usfm', function(err, data) {
+			if(err) throw err;
+
+	  		var input = data;
+	  		var output = parser.validator(input);
+	  		assert.equal(output,false);
+	  	});
+  	});
+
+	it('C is a mandatory_marker',function(){
+  		fs.readFile('test/test_simple_noC_wrong.usfm', function(err, data) {
+			if(err) throw err;
+
+	  		var input = data;
+	  		var output = parser.validator(input);
+	  		assert.equal(output,false);
+	  	});
+  	});
+
+  	it('P is a mandatory_marker',function(){
+  		fs.readFile('test/test_simple_noP_wrong.usfm', function(err, data) {
+			if(err) throw err;
+
+	  		var input = data;
+	  		var output = parser.validator(input);
+	  		assert.equal(output,false);
+	  	});
+  	});
+
+  	it('V is a mandatory_marker',function(){
+  		fs.readFile('test/test_simple_noV_wrong.usfm', function(err, data) {
+			if(err) throw err;
+
+	  		var input = data;
+	  		var output = parser.validator(input);
+	  		assert.equal(output,false);
+	  	});
+  	});
+  });
+
+  describe("#different_languages_and_sources",function(){
+  	it("Bengali from E-Bible",function(){
+  		fs.readFile('test/test_bengaliFromEbible_correct.usfm', function(err, data) {
+			if(err) throw err;
+
+	  		var input = data;
+	  		var output = parser.validator(input);
+	  		assert.equal(output,true);
+	  	});
+  	});
+
+  	it("Japanese from E-Bible",function(){
+  		fs.readFile('test/test_JapaneseFromEbible_correct.usfm', function(err, data) {
+			if(err) throw err;
+
+	  		var input = data;
+	  		var output = parser.validator(input);
+	  		assert.equal(output,true);
+	  	});
+  	});
+
+  	it("Brazilian from E-Bible",function(){
+  		fs.readFile('test/test_Maxakal\'i-BrazilFromEbible_correct.usfm', function(err, data) {
+			if(err) throw err;
+
+	  		var input = data;
+	  		var output = parser.validator(input);
+	  		assert.equal(output,true);
+	  	});
+  	});
+
+  	it("Tamil from E-Bible",function(){
+  		fs.readFile('test/test_tamilFromEbible_correct.usfm', function(err, data) {
+			if(err) throw err;
+
+	  		var input = data;
+	  		var output = parser.validator(input);
+	  		assert.equal(output,true);
+	  	});
+  	});
+  	
+  	it("OET from freelygiven",function(){
+  		fs.readFile('test/test_OETfromfreelygiven_correct.usfm', function(err, data) {
+			if(err) throw err;
+
+	  		var input = data;
+	  		var output = parser.validator(input);
+	  		assert.equal(output,true);
+	  	});
+  	});
+
+  	it("Psalms from OEB",function(){
+  		fs.readFile('test/test_PsalmsOEB_correct.usfm', function(err, data) {
+			if(err) throw err;
+
+	  		var input = data;
+	  		var output = parser.validator(input);
+	  		assert.equal(output,true);
+	  	});
+  	});
+
   });
 });
