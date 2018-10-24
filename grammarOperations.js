08332d2c02ef3a35efc963b5a070ac45d0dd30f4
kavitharaju <kavitharaju18@gmail.com>
2018-10-24 10:39:28 +0530

    started implemeting operations

--- grammarOperations.js
+++ grammarOperations.js
@@ -1,59 +1,127 @@
-const fs = require('fs');
-const ohm = require('ohm-js');
-const join = require('path').join;
+const fs = require('fs')
+const ohm = require('ohm-js')
+const join = require('path').join
 
-var contents = fs.readFileSync(join(__dirname, 'usfm.ohm'));
+var contents = fs.readFileSync(join(__dirname, 'usfm.ohm'))
 
-var bib  = ohm.grammars(contents).usfmBible;
+var bib = ohm.grammars(contents).usfmBible
 
-var sem = bib.createSemantics();
+var sem = bib.createSemantics()
 
-console.log("Initializing grammar");
+console.log('Initializing grammar')
 
+// Actions enabling the matched string to be converted to JSON(to be changed according to new grammar)
 
-var res =[]		
-
-
-//Actions enabling the matched string to be converted to JSON(to be changed according to new grammar)
-
-sem.addOperation('jsonCompose', {
-	File : function(e){
-			e.jsonCompose();
-				 
-			return res;
-	},
-
-	Valid_content : function(e){
-		e.jsonCompose()
-	},
-
-	simple_element : function(_,m){
-		res.push({"tag": m.sourceString });
-	},
-
-	text_element : function(_,m,_,t){
-		res.push({"tag": m.sourceString, "content" : t.sourceString});
-	},
-
-	opt_text_element : function(_,m,_,t){
-		res.push({"tag": m.sourceString, "content" : t.sourceString});
-	},
-
-	double_arg_element : function(_,m,_,v,_,t){
-		res.push({"tag": m.sourceString, "value" : v.sourceString,
-										"text" : t.sourceString})
-	}
-
-});
-
-exports.match = function (str){
-			try{
-
-				var matchObj = bib.match(str);
-				var adaptor = sem(matchObj);
-				return adaptor.jsonCompose();
-			}catch(err){
-				return matchObj
-			}
+res = {
+	'metaData' : {},
+	'content' : {}
 }
 
+chapterHeaderVar = {}
+chapterContentVar = {}
+
+sem.addOperation('jsonCompose', {
+  File: function (e) {
+    e.jsonCompose()
+
+    return res
+  },
+
+  scripture: function (metaData, content) {
+    metaData.jsonCompose()
+    content.jsonCompose()
+  },
+
+  metaData: function (bookIdentification, bh, bt, bit, biet, bcl) {
+    bookIdentification.jsonCompose(),
+    bh.jsonCompose(),
+    bt.jsonCompose(),
+    bit.jsonCompose(),
+    biet.jsonCompose(),
+    bcl.jsonCompose()
+    
+  },
+
+  bookIdentification: function (idElm, usfmElm) {
+    var elmt = {'id': idElm.jsonCompose(),
+      'usfm': usfmElm.jsonCompose()}
+    res['metaData'].push({'bookIdentification':elmt})
+  },
+
+  bookHeaders : function (bh) {
+  	if (!res['metaData']['bookHeaders']) {
+  		res['metaData'].push({'bookHeaders':[]})
+  	}
+  	var elmt = bh.jsonCompose()
+  	res['metaData']['bookHeaders'].add(elmt)
+  },
+
+  bookTitles : function (bt) {
+  	if (!res['metaData']['bookTitles']) {
+  		res['metaData'].push({'bookTitles':[]})
+  	}
+  	var elmt = bt.jsonCompose()
+  	res['metaData']['bookTitles'].add(elmt)
+  },
+
+  bookIntroductionTitles : function (bit) {
+  	if (!res['metaData']['bookIntroductionTitles']) {
+  		res['metaData'].push({'bookIntroductionTitles':[]})
+  	}
+  	var elmt = bit.jsonCompose()
+  	res['metaData']['bookIntroductionTitles'].add(elmt)
+  },
+
+  bookIntroductionEndTitles : function (biet) {
+  	if (!res['metaData']['bookIntroductionEndTitles']) {
+  		res['metaData'].push({'bookIntroductionEndTitles':[]})
+  	}
+  	var elmt = biet.jsonCompose()
+  	res['metaData']['bookIntroductionEndTitles'].add(elmt)
+  },
+
+  bookChapterLabel : function (bcl) {
+  	var elmt = {'bookChapterLabel': bcl.jsonCompose()}
+  	res['metaData'].push(elmt)
+  },
+
+  content : function (chapH, chapC) {
+  	if (!res['content']['chapters']) {
+  		res['metaData'].push({'chapters':[]})
+  	}
+  	res['content']['chapters'].add({'chapterHeader':chapH.jsonCompose(),
+  	  'chapterContent':chapC.jsonCompose() })
+  },
+
+  chapterHeader : function (c,extra) {
+  	chapterHeaderVar = {}
+  	c.jsonCompose()
+  	extra.jsonCompose()
+  	return chapterHeaderVar
+
+  },
+
+  chapterContent : function (s) {
+  	chapterContentVar = {}
+  	s.jsonCompose()
+  	return chapterContentVar
+  },
+
+  
+
+
+
+
+
+
+})
+
+exports.match = function (str) {
+  try {
+    var matchObj = bib.match(str)
+    var adaptor = sem(matchObj)
+    return adaptor.jsonCompose()
+  } catch (err) {
+    return matchObj
+  }
+}
