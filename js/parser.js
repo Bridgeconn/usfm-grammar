3059f19794c798aaaac35e7ad1b4d8f0690b1fde
kavitharaju <kavitharaju18@gmail.com>
2019-08-02 11:12:13 +0530

    export validate methode also

--- js/parser.js
+++ js/parser.js
@@ -1,5 +1,7 @@
 const parseUSFM = require('./USFMparser.js').parseUSFM
 const parseJSON = require('./JSONparser.js').parseJSON
+const validate  = require('./USFMparser.js').validate
 
 exports.parseUSFM =parseUSFM
 exports.parseJSON =parseJSON
+exports.validate = validate
