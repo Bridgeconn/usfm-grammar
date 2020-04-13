6bb7a4dcfbebbd1e116c9e69b70de66a9add85d2
kavitharaju <kavitharaju18@gmail.com>
2020-04-13 14:57:29 +0530

    add a script to automatically create the js string corresponding to OHM grammar

---
+++ js/convert_ohm_to_jsString.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+
+var grammarString = '***'
+const commentsOneLine = new RegExp('//.*', 'g')
+const commentsMultiLine = new RegExp('/[*][^*]*[*]/','g')
+const multiLines = new RegExp('[\n\r][\n\r]+','g')
+const backslash = new RegExp('\\\\','g')
+const newline = new RegExp('\n','g')
+
+fs.readFile('grammar/usfm.ohm','utf-8', function (err, data) {
+  if (err) { throw err }
+  grammarString = data.replace(commentsOneLine,'')
+  grammarString = grammarString.replace(commentsMultiLine, '')
+  grammarString = grammarString.replace(multiLines,'\n')
+  grammarString = grammarString.replace(backslash,'\\\\')
+  grammarString = grammarString.replace(newline,'\\n')
+  
+  var fileContent = "exports.contents = '" + grammarString + "'"
+  fs.writeFile('js/usfm.ohm.js', fileContent, (err) => {
+	    // throws an error, you could also catch it here
+	    if (err) console.log(err.message);
+
+	    // success case, the file was saved
+	    console.log('grammarstring written!');
+	});
+})
+
+
+
