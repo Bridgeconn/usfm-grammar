b196aa47a5ee9eedca3dc1453a2f135a763e098c
kavitharaju <kavitharaju18@gmail.com>
2020-05-28 12:04:52 +0530

    optimize CLI code

--- cli.js
+++ cli.js
@@ -1,37 +1,30 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
 const grammar = require('./js/main.js');
-const fs = require('fs')
+
+/* eslint no-console: ["error", { allow: ["log"] }] */
 
 const argLength = process.argv.length;
-const file = process.argv[argLength-1];
+const file = process.argv[argLength - 1];
 const inputFile = fs.readFileSync(file, 'utf-8');
-if (process.argv.includes("--REVERSE")) {
-	const myJsonParser = new grammar.JSONParser(inputFile);
-	console.log(myJsonParser.toUSFM(inputFile));
+if (process.argv.includes('--REVERSE')) {
+  const myJsonParser = new grammar.JSONParser(inputFile);
+  console.log(myJsonParser.toUSFM(inputFile));
 } else {
-	if (process.argv.includes("--LEVEL.RELAXED") || process.argv.includes("--RELAXED")) {
-		const myUsfmParser = new grammar.USFMParser(inputFile, grammar.LEVEL.RELAXED);
-		if (process.argv.includes("--CSV") || process.argv.includes("--csv")) {
-			console.log(myUsfmParser.toCSV());
-		} else if (process.argv.includes("--TSV") || process.argv.includes("--tsv")) {
-			console.log(myUsfmParser.toTSV());
-		} else if (process.argv.includes("--FILTER.SCRIPTURE") || process.argv.includes("--SCRIPTURE")) {
-			console.log(JSON.stringify(myUsfmParser.toJSON(grammar.FILTER.SCRIPTURE),null,2));	
-		} else {
-			console.log(JSON.stringify(myUsfmParser.toJSON(),null,2));
-		}
-	} else {
-		const myUsfmParser = new grammar.USFMParser(inputFile);
-		if (process.argv.includes("--CSV") || process.argv.includes("--csv")) {
-			console.log(myUsfmParser.toCSV());
-		} else if (process.argv.includes("--TSV") || process.argv.includes("--tsv")) {
-			console.log(myUsfmParser.toTSV());
-		} else if (process.argv.includes("--FILTER.SCRIPTURE") || process.argv.includes("--SCRIPTURE")) {
-			console.log(JSON.stringify(myUsfmParser.toJSON(grammar.FILTER.SCRIPTURE),null,2));	
-		} else {
-			console.log(JSON.stringify(myUsfmParser.toJSON(),null,2));
-		}
-
-	}
+  let myUsfmParser = null;
+  if (process.argv.includes('--LEVEL.RELAXED') || process.argv.includes('--RELAXED')) {
+    myUsfmParser = new grammar.USFMParser(inputFile, grammar.LEVEL.RELAXED);
+  } else {
+    myUsfmParser = new grammar.USFMParser(inputFile);
+  }
+  if (process.argv.includes('--CSV') || process.argv.includes('--csv')) {
+    console.log(myUsfmParser.toCSV());
+  } else if (process.argv.includes('--TSV') || process.argv.includes('--tsv')) {
+    console.log(myUsfmParser.toTSV());
+  } else if (process.argv.includes('--FILTER.SCRIPTURE') || process.argv.includes('--SCRIPTURE')) {
+    console.log(JSON.stringify(myUsfmParser.toJSON(grammar.FILTER.SCRIPTURE), null, 2));
+  } else {
+    console.log(JSON.stringify(myUsfmParser.toJSON(), null, 2));
+  }
 }
