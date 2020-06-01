e0187874d4eaaadfa79fd1619fe86717853e5cfa
kavitharaju <kavitharaju18@gmail.com>
2020-06-01 13:42:51 +0530

    add required file-path argument and error handling in CLI

--- cli.js
+++ cli.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 const fs = require('fs');
-const grammar = require('./js/main.js');
-const argv = require('yargs')
+const { argv } = require('yargs')
+  .command('* <file-path>', 'parse and convert the input file', (yargs) => {
+    yargs.positional('file-path', {
+      describe: 'the input USFM or JSON file to be parsed and converted',
+    });
+  })
   .alias('l', 'level')
   .describe('l', 'specify the level of strictness in parsing')
   .choices('l', ['relaxed'])
@@ -12,25 +16,38 @@ const argv = require('yargs')
   .alias('o', 'output')
   .describe('o', 'specify the fully qualified file path for output.')
   .alias('h', 'help')
-  .help('help')
-  .argv
+  .help('help');
+const grammar = require('./js/main.js');
 
-/* eslint no-console: ["error", { allow: ["log"] }] */
+/* eslint no-console: ["error", { allow: ["log", "error"] }] */
 
-const file = argv._[0];
-const inputFile = fs.readFileSync(file, 'utf-8');
+const file = argv['file-path'];
+let inputFile = null;
+try {
+  inputFile = fs.readFileSync(file, 'utf-8');
+} catch (e) {
+  console.error('Error reading input file');
+  console.error(e.message);
+  process.exit(1);
+}
 let isJson = false;
 let output = '';
 let jsonInput = null;
 try {
-    jsonInput = JSON.parse(inputFile);
-    isJson = true;
+  jsonInput = JSON.parse(inputFile);
+  isJson = true;
 } catch (e) {
-    isJson = false;
+  isJson = false;
 }
 if (argv.format === 'usfm' || isJson) {
   const myJsonParser = new grammar.JSONParser(jsonInput);
-  output = myJsonParser.toUSFM(inputFile);
+  try {
+    output = myJsonParser.toUSFM(inputFile);
+  } catch (e) {
+    console.error('Error parsing the input JSON.');
+    console.error(e.message);
+    process.exit(1);
+  }
 } else {
   let myUsfmParser = null;
   if (argv.level === 'relaxed' || argv.l === 'relaxed') {
@@ -38,23 +55,35 @@ if (argv.format === 'usfm' || isJson) {
   } else {
     myUsfmParser = new grammar.USFMParser(inputFile);
   }
-  if (argv.format === 'csv') {
-    output = myUsfmParser.toCSV();
-  } else if (argv.format === 'tsv') {
-    output = myUsfmParser.toTSV();
-  } else if (argv.filter === 'scripture') {
-    output = JSON.stringify(myUsfmParser.toJSON(grammar.FILTER.SCRIPTURE), null, 2);
-  } else {
-    output = JSON.stringify(myUsfmParser.toJSON(), null, 2);
+  try {
+    if (argv.format === 'csv') {
+      output = myUsfmParser.toCSV();
+    } else if (argv.format === 'tsv') {
+      output = myUsfmParser.toTSV();
+    } else if (argv.filter === 'scripture') {
+      output = JSON.stringify(myUsfmParser.toJSON(grammar.FILTER.SCRIPTURE), null, 2);
+    } else {
+      output = JSON.stringify(myUsfmParser.toJSON(), null, 2);
+    }
+  } catch (e) {
+    console.error('Error parsing the input USFM.');
+    console.error(e.message);
+    process.exit(1);
   }
 }
 
-if ( Object.prototype.hasOwnProperty.call(argv, 'o') || Object.prototype.hasOwnProperty.call(argv, 'output')) {
+if (Object.prototype.hasOwnProperty.call(argv, 'o') || Object.prototype.hasOwnProperty.call(argv, 'output')) {
   let filePath = argv.o;
   if (!filePath) {
     filePath = argv.output;
   }
-  fs.writeFileSync(filePath, output)
+  try {
+    fs.writeFileSync(filePath, output);
+  } catch (e) {
+    console.error('Error writting output file');
+    console.error(e.message);
+    process.exit(1);
+  }
 } else {
-  console.log(output)
+  console.log(output);
 }
