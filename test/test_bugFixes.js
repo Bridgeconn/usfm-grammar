87471a544b2a19bfca2db0bd5902bcd135d5645b
kavitharaju <kavitharaju18@gmail.com>
2020-08-11 11:04:21 +0530

    link githhub issues with bug fix tests. And add one more test

--- test/test_bugFixes.js
+++ test/test_bugFixes.js
@@ -8,6 +8,8 @@ describe('Test bug fixes', () => {
   });
 
   it('text with "id XXX" should not capitalize XXX', () => {
+  	// ISSUE: On parsing the given usfm file the first verse shows mixed case - crEATed
+  	// https://github.com/Bridgeconn/usfm-grammar/issues/77
     let inputUsfm = '\\id GEN\n\\c 1\n\\p\n\\v 1 verse one\n\\v 2 verse two did have an id in it.'
     const usfmParser = new grammar.USFMParser(inputUsfm);
     let jsonOutput = usfmParser.toJSON();
@@ -22,6 +24,7 @@ describe('Test bug fixes', () => {
   });
   
   it('nested char attribute element parsing', () => {
+    // ISSUE: The contents within nested marker with attribute was shown as  [object object]
     let inputUsfm = '\\id GEN\n\\c 1\n\\p\n\\v 1 verse one\n\\v 2 ...\\w herbs|strong="H6212"\\w* \\w yielding|strong="H2232"\\w* \\w seed|strong="H2233"\\w*, \\add \\+w and|strong="H430"\\+w*\\add* fruit-trees \\w bearing|strong="H6213"\\w* \\w fruit|strong="H6529"\\w* \\w after|strong="H5921"\\w* \\w their|strong="H5921"\\w* \\w kind|strong="H4327"\\w*,...'
     const usfmParser = new grammar.USFMParser(inputUsfm);
     let jsonOutput = usfmParser.toJSON();
@@ -35,6 +38,13 @@ describe('Test bug fixes', () => {
     assert.strictEqual(outputUsfm.replace(/[\s\n\r]/g,''), inputUsfm.replace(/[\s\n\r]/g,''));  
   });
 
-
+  it('No new space added while concatinating verseText at punctuations', () => {
+  	// ISSUE: Parsing the usfm file gives the output with spaces in between
+  	// https://github.com/Bridgeconn/usfm-grammar/issues/76
+  	let inputUsfm = '\\id GEN\n\\c 1\n\\p\n\\v 1 verse one\n\\v 2 verse two\\p, which has a comma at the text break point.';
+    const usfmParser = new grammar.USFMParser(inputUsfm);
+    let jsonOutput = usfmParser.toJSON();
+	assert.strictEqual(jsonOutput.chapters[0].contents[2].verseText, 'verse two, which has a comma at the text break point.');
+  });
 
 });
