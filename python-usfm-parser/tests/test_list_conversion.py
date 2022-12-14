8f3d02941ad84154054a4e6611b6f89dd83f55c5
Kavitha.Raju <kavitha.raju@bridgeconn.com>
2022-12-14 13:41:21 +0530

    test to_list conversion

---
+++ python-usfm-parser/tests/test_list_conversion.py
@@ -0,0 +1,19 @@
+'''Test the to_dict or json conversion API'''
+import pytest
+
+from tests import all_usfm_files, initialise_parser, doubtful_usfms, negative_tests
+from src.usfm_grammar import Filter
+
+test_files = all_usfm_files.copy()
+for file in doubtful_usfms+negative_tests:
+    if file in test_files:
+        test_files.remove(file)
+
+@pytest.mark.parametrize('file_path', test_files)
+@pytest.mark.timeout(30)
+def test_list_converions_without_filter(file_path):
+    '''Tests if input parses without errors'''
+    test_parser = initialise_parser(file_path)
+    assert not test_parser.errors, test_parser.errors
+    usfm_dict = test_parser.to_list([Filter.SCRIPTURE_TEXT])
+    assert isinstance(usfm_dict, list)
