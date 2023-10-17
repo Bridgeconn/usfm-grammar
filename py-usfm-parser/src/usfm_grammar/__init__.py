bbe04e1ff05f67767ed5ee7c40ef902c654e5b2b
Kavitha.Raju <kavitha.raju@bridgeconn.com>
2023-10-17 15:25:50 +0530

    Move usj-to-usfm conversion API into the main USFMParser class itself(in constructor)

--- py-usfm-parser/src/usfm_grammar/__init__.py
+++ py-usfm-parser/src/usfm_grammar/__init__.py
@@ -6,6 +6,5 @@ from usfm_grammar import usfm_generator
 Filter = usfm_parser.Filter
 Format = usfm_parser.Format
 USFMParser = usfm_parser.USFMParser
-USFMGenerator = usfm_generator.USFMGenerator
 
 __version__ = "3.0.0-beta.2"
--- py-usfm-parser/src/usfm_grammar/__main__.py
+++ py-usfm-parser/src/usfm_grammar/__main__.py
@@ -11,14 +11,60 @@ all_markers = []
 for member in Filter:
     all_markers += member.value
 
+def handle_input_file(arg_parser):
+    '''If initialsing with USFM or USJ?'''
+    infile = arg_parser.parse_args().infile
+    input_format = arg_parser.parse_args().in_format
+    with open(infile, 'r', encoding='utf-8') as usfm_file:
+        file_content = usfm_file.read()
+
+    if input_format == Format.JSON or infile.split(".")[-1] in ['json', 'usj']:
+        usj_obj = json.loads(file_content)
+        my_parser = USFMParser(from_usj=usj_obj)
+    elif input_format == Format.USFM:
+        my_parser = USFMParser(file_content)
+    else:
+        raise Exception("Un-recognized input_format!")
+    return my_parser
+
+def handle_include_exclude_options(arg_parser):
+    '''Process list of markers and ENUMs'''
+    exclude_markers = arg_parser.parse_args().exclude_markers
+    include_markers = arg_parser.parse_args().include_markers
+
+    filter_names =  [member.name for member in Filter]
+    if exclude_markers is None:
+        updated_exclude_markers = None
+    else:
+        updated_exclude_markers = []
+        for itm in exclude_markers:
+            if itm.upper() in filter_names:
+                updated_exclude_markers += Filter[itm.upper()]
+            else:
+                updated_exclude_markers.append(itm.lower().replace("\\", ""))
+    if include_markers is None:
+        updated_include_markers = None
+    else:
+        updated_include_markers = []
+        for itm in include_markers:
+            if itm.upper() in filter_names:
+                updated_include_markers += Filter[itm.upper()]
+            else:
+                updated_include_markers.append(itm.lower().replace("\\", ""))
+    return updated_exclude_markers, updated_include_markers
+
 
 def main(): #pylint: disable=too-many-locals
     '''handles the command line requests'''
     arg_parser = argparse.ArgumentParser(
         description='Uses the tree-sitter-usfm grammar to parse and convert USFM to '+\
         'Syntax-tree, JSON, CSV, USX etc.')
-    arg_parser.add_argument('infile', type=str, help='input usfm file')
-    arg_parser.add_argument('--format', type=str, help='output format',
+    arg_parser.add_argument('infile', type=str, help='input usfm or usj file')
+
+    arg_parser.add_argument('--in_format', type=str, help='input file format',
+                            choices=[Format.USFM.value, Format.JSON.value],
+                            default=Format.USFM.value)
+    arg_parser.add_argument('--out_format', type=str, help='output format',
                             choices=[itm.value for itm in Format],
                             default=Format.JSON.value)
     arg_parser.add_argument('--include_markers', type=str,
@@ -44,52 +90,29 @@ def main(): #pylint: disable=too-many-locals
                             'from different components, or not',
                             action='store_true')
 
-    infile = arg_parser.parse_args().infile
-    output_format = arg_parser.parse_args().format
-    exclude_markers = arg_parser.parse_args().exclude_markers
-    include_markers = arg_parser.parse_args().include_markers
 
-    with open(infile, 'r', encoding='utf-8') as usfm_file:
-        file_content = usfm_file.read()
-
-    my_parser = USFMParser(file_content)
+    my_parser = handle_input_file(arg_parser)
 
     if my_parser.errors and not arg_parser.parse_args().ignore_errors:
         err_str = "\n\t".join([":".join(err) for err in my_parser.errors])
         print(f"Errors present:\n\t{err_str}")
         sys.exit(1)
 
-    filter_names =  [member.name for member in Filter]
-    if exclude_markers is None:
-        updated_exclude_markers = None
-    else:
-        updated_exclude_markers = []
-        for itm in exclude_markers:
-            if itm.upper() in filter_names:
-                updated_exclude_markers += Filter[itm.upper()]
-            else:
-                updated_exclude_markers.append(itm.lower().replace("\\", ""))
-    if include_markers is None:
-        updated_include_markers = None
-    else:
-        updated_include_markers = []
-        for itm in include_markers:
-            if itm.upper() in filter_names:
-                updated_include_markers += Filter[itm.upper()]
-            else:
-                updated_include_markers.append(itm.lower().replace("\\", ""))
+    exclude_markers, include_markers = handle_include_exclude_options(arg_parser)
+
+    output_format = arg_parser.parse_args().out_format
 
     match output_format:
         case Format.JSON:
             dict_output = my_parser.to_usj(
-                exclude_markers=updated_exclude_markers,
-                include_markers=updated_include_markers,
+                exclude_markers=exclude_markers,
+                include_markers=include_markers,
                 ignore_errors=True)
             print(json.dumps(dict_output, indent=4, ensure_ascii=False))
         case Format.CSV:
             table_output = my_parser.to_list(
-                exclude_markers=updated_exclude_markers,
-                include_markers=updated_include_markers,
+                exclude_markers=exclude_markers,
+                include_markers=include_markers,
                 ignore_errors=True)
             outfile = sys.stdout
             writer = csv.writer(outfile,
@@ -104,6 +127,8 @@ def main(): #pylint: disable=too-many-locals
             print(my_parser.to_markdown())
         case Format.ST:
             print(my_parser.to_syntax_tree(ignore_errors=True))
+        case Format.USFM:
+            print(my_parser.usfm)
         case _:
             raise Exception(f"Un-recognized output format:{output_format}!")
 
