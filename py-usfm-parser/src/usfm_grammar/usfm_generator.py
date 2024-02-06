282f9dd546e261d0bcc1cfa1abeed6c7b86a7afd
Kavitha.Raju <kavitha.raju@bridgeconn.com>
2024-02-06 18:27:48 +0530

    Build USX to USFM conversion

--- py-usfm-parser/src/usfm_grammar/usfm_generator.py
+++ py-usfm-parser/src/usfm_grammar/usfm_generator.py
@@ -6,17 +6,22 @@ CLOSING_USJ_TYPES = ['char', 'note', 'figure']
 NON_ATTRIB_USJ_KEYS = ['type', 'marker', 'content', 'number', 'sid',
                         'code', 'caller', 'align',
                         'version', 'altnumber', 'pubnumber', 'category']
+NON_ATTRIB_USX_KEYS =  ['number', 'code', 'caller', 'align', "sid", "eid",
+                        "style", "closed", "vid", "status",'version', 
+                        'altnumber', 'pubnumber', 'category']
+NO_NEWLINE_USX_TYPES = ['char', 'note', "cell", "figure", "usx", "book", "optbreak"]
+CLOSING_USX_TYPES = ['char', 'note', 'figure', "ms"]
 
 class USFMGenerator:
     '''Combines the different methods that generate USFM from other formats in one class'''
     def __init__(self):
         self.usfm_string = ''
 
-    def is_valid_usfm(self, usfm_string: dict = None) -> bool:
-        '''Check the generated or passed USFM's correctness using the grammar'''
-        if usfm_string is None:
-            usfm_string = self.usfm_string
-        return False
+    # def is_valid_usfm(self, usfm_string: str = None) -> bool:
+    #     '''Check the generated or passed USFM's correctness using the grammar'''
+    #     if usfm_string is None:
+    #         usfm_string = self.usfm_string
+    #     return False
 
     def usj_to_usfm(self, usj_obj: dict, nested=False) -> None: # pylint: disable=too-many-statements, too-many-branches
         '''Traverses through the dict/json and uses 'type' field to form USFM elements'''
@@ -82,7 +87,91 @@ class USFMGenerator:
             else:
                 self.usfm_string += "\n"
 
-    # def usx_to_usfm(self, usx_xml_tree) -> str: # should we call it just from_usx() instead
-    #     '''Traverses xml tree and converts nodes to usfm elements
-    #     based on type and style fields'''
-    #     return self.usfm_string
+    def usx_to_usfm(self, xml_obj, nested=False) -> str: # should we call it just from_usx() instead
+        '''Traverses xml tree and converts nodes to usfm elements
+        based on type and style fields'''
+        if isinstance(xml_obj, str):
+            self.usfm_string += xml_obj
+            return
+        obj_type = xml_obj.tag
+        marker = None
+        usfm_attributes = []
+        if obj_type in ["verse", "chapter"] and "eid" in xml_obj.attrib:
+            return
+        if obj_type not in NO_NEWLINE_USX_TYPES:
+            self.usfm_string += "\n"
+        if obj_type == "optbreak":
+            if self.usfm_string != "" and self.usfm_string[-1] not in ["\n", "\r", " ", "\t"]:
+                self.usfm_string += " "
+            self.usfm_string += "// "
+        if "style" in xml_obj.attrib:
+            marker = xml_obj.attrib["style"]
+            if nested and obj_type=="char":
+                marker = "+"+marker
+            self.usfm_string += f"\\{marker} "
+        if "code" in xml_obj.attrib:
+            self.usfm_string += xml_obj.attrib['code']
+        if "number" in xml_obj.attrib:
+            self.usfm_string += f"{xml_obj.attrib['number']} "
+        if "caller" in xml_obj.attrib:
+            self.usfm_string += f"{xml_obj.attrib['caller']} "
+        if "altnumber" in xml_obj.attrib:
+            if obj_type == "verse":
+                self.usfm_string += f"\\va {xml_obj.attrib['altnumber']}\\va*"
+            elif obj_type == "chapter":
+                self.usfm_string += f"\n\\ca {xml_obj.attrib['altnumber']}\\ca*"
+        if "pubnumber" in xml_obj.attrib:
+            if obj_type == "verse":
+                self.usfm_string += f"\\vp {xml_obj.attrib['pubnumber']}\\vp*"
+            elif obj_type == "chapter":
+                self.usfm_string += f"\n\\cp {xml_obj.attrib['pubnumber']}"
+        if "category" in xml_obj.attrib:
+            self.usfm_string += f"\n\\cat {xml_obj.attrib['category']} \\cat*"
+        if xml_obj.text:
+            if self.usfm_string != "" and self.usfm_string[-1] not in ["\n", "\r", " ", "\t"]:
+                self.usfm_string += " "
+            self.usfm_string += xml_obj.text.strip()
+        for child in xml_obj.getchildren():
+            if obj_type in ["char"]:
+                self.usx_to_usfm(child, nested=True)
+            else:
+                self.usx_to_usfm(child, nested=False)
+            if child.tail:
+                if self.usfm_string !="" and self.usfm_string[-1] not in ["\n", "\r", " ", "\t"]:
+                    self.usfm_string += " "
+                self.usfm_string += child.tail.strip()
+        for key in xml_obj.attrib:
+            val = xml_obj.attrib[key]
+            val = val.replace('"', '')
+            if key == "file" and obj_type=="figure":
+                usfm_attributes.append(f'src="{val}"')
+            elif key not in NON_ATTRIB_USX_KEYS:
+                usfm_attributes.append(f'{key}="{val}"')
+            if key in ['sid', 'eid'] and obj_type=="ms":
+                usfm_attributes.append(f'{key}="{val}"')
+        if len(usfm_attributes) > 0:
+            self.usfm_string += "|"
+            self.usfm_string += " ".join(usfm_attributes)
+
+        if (("closed" in xml_obj.attrib and xml_obj.attrib['closed']=="true") 
+            or obj_type in CLOSING_USX_TYPES
+            or len(usfm_attributes)>0):
+            # if not ("closed" in xml_obj.attrib and xml_obj.attrib['closed']=="false"):
+            if obj_type == "ms":
+                self.usfm_string += "\\*"
+            else:
+                self.usfm_string += f"\\{marker}*"
+        
+if __name__ == "__main__":
+    from lxml import etree
+
+    test_xml_file = "../../../tests/basic/cross-refs/origin.xml"
+
+    with open(test_xml_file, 'r', encoding='utf-8') as usx_file:
+        usx_str = usx_file.read()
+        root = etree.fromstring(usx_str)
+
+        gen = USFMGenerator()
+
+        gen.usx_to_usfm(root)
+        print(gen.usfm_string)
