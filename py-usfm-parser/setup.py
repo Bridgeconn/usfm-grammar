e2d3ff30d0aee4d6fb7ea0e13038894487289d4c
Kavitha.Raju <kavitha.raju@bridgeconn.com>
2024-05-22 20:38:55 +0530

    Bump version: 3.0.0-beta.5 → 3.0.0-beta.6

--- py-usfm-parser/setup.py
+++ py-usfm-parser/setup.py
@@ -7,7 +7,7 @@ class BinaryDistribution(Distribution):
 
 setup(
     name="usfm-grammar",  # Required
-    version="3.0.0-beta.5",  # Required
+    version="3.0.0-beta.6",  # Required
     python_requires=">=3.10",
     install_requires=["tree-sitter==0.21.3", "lxml==5.2.2"],  # Optional
     package_data={  # Optional
