5f52464812983e3dc9de4a8907e60a4a6fa64143
Kavitha.Raju <kavitha.raju@bridgeconn.com>
2024-08-19 14:24:27 +0530

    Fix the basic test scripts to use files in dist/ folder generated after npm run build command

--- js-usfm-parser/test.cjs
+++ js-usfm-parser/test.cjs
@@ -1,4 +1,5 @@
-import USFMParser from './dist/esm/index.js';
+// To use in this repo temporarily remove the "type": "module" in package.json
+const USFMParser = require("../dist/cjs/index.cjs").default;
 
 (async () => {
   await USFMParser.init();
@@ -8,4 +9,3 @@ import USFMParser from './dist/esm/index.js';
   const usfm = usfmParser.usjToUsfm(output)
   console.log({ usfm })
 })();
-
