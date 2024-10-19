6689d2bcdc024984126a8b3e41b3df66bfa8fba5
kavitharaju <kavitharaju18@gmail.com>
2024-10-19 13:19:24 +0530

    Web: Add tests for roundtripping USFM via USX

--- web-usfm-parser/test.js
+++ web-usfm-parser/test.js
@@ -1,5 +1,6 @@
 import {USFMParser} from './src/index.js';
 import { readFile } from 'fs/promises';
+import { DOMParser } from 'xmldom';
 
 (async () => {
   await USFMParser.init("tree-sitter-usfm.wasm", "tree-sitter.wasm");
@@ -12,15 +13,30 @@ import { readFile } from 'fs/promises';
   // const output2 = usfmParser.usfm;
   // console.log({ output2 });
 
-  const filePath = "../tests/usfmjsTests/missing_verses/origin.usfm";
+  // const filePath = "../tests/usfmjsTests/missing_verses/origin.usfm";
+  // const content = await readFile(filePath, 'utf-8'); // Specify encoding
+  // console.log(content);
+
+  // await USFMParser.init("tree-sitter-usfm.wasm", "tree-sitter.wasm");
+  // const usfmParser = new USFMParser(content);
+  // const output = usfmParser.toUSJ(null, null, true);
+  // console.log({output})
+  
+  const filePath = "../tests/usfmjsTests/missing_verses/origin.xml";
   const content = await readFile(filePath, 'utf-8'); // Specify encoding
   console.log(content);
+  console.log("*************************");
 
-  await USFMParser.init("tree-sitter-usfm.wasm", "tree-sitter.wasm");
-  const usfmParser = new USFMParser(content);
+  const doc = new DOMParser().parseFromString(content);
+  const usfmParser = new USFMParser(null, null, doc);
+  console.log(usfmParser.usfm)
+  console.log("*************************");
+
   const output = usfmParser.toUSJ(null, null, true);
   console.log({output})
-  
+  console.log("*************************");
+
+
 
 })();
 
