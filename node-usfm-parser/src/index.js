2eb48d931e33d83ebabc31c0d956bf0356c895f8
kavitharaju <kavitharaju18@gmail.com>
2024-11-05 09:52:54 +0530

    Implement validation and autofix in node

--- node-usfm-parser/src/index.js
+++ node-usfm-parser/src/index.js
@@ -1,5 +1,7 @@
 const {USFMParser, Filter, Format } = require("./usfmParser");
+const {Validator} = require("./validator");
 
 exports.USFMParser = USFMParser;
 exports.Filter = Filter;
 exports.Format = Format;
+exports.Validator = Validator;
