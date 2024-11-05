b209329c2e603e4543c9e36b529ea916955a6240
kavitharaju <kavitharaju18@gmail.com>
2024-11-05 11:44:59 +0530

    Add tests for USJ validation in node, web and python

--- node-usfm-parser/test/test_auto_fix.js
+++ node-usfm-parser/test/test_auto_fix.js
@@ -5,6 +5,7 @@ const {Validator} = require("../src/index");
 
 const TEST_DIR = "../tests";
 const allUSFMFiles = glob.sync(TEST_DIR+'/autofix/*');
+const sampleUSJs = glob.sync(TEST_DIR+'/specExamples/*/origin.json')
 
 describe("Try autofixing errors in USFM", () => {
 
@@ -19,6 +20,35 @@ describe("Try autofixing errors in USFM", () => {
         const secondTest = testVaidator.isValidUSFM(fixedUsfm);
         // assert.ok(!firstTest);
         assert.ok(secondTest);
+      });
+    });
+
+});
+
+describe("Validate USJ", () => {
+    sampleUSJs.forEach(function(value) {
+      it(`Validate ${value}`, (inputUsjPath=value) => {
+        //Tests if input parses without errors
+        const testVaidator = new Validator()
+        assert(testVaidator instanceof Validator)
+        const inputUsj = fs.readFileSync(inputUsjPath, 'utf8')
+        const usj = JSON.parse(inputUsj);
+        // assert.ok(!firstTest);
+        assert.ok(testVaidator.isValidUSJ(usj));
+      });
+    });
+
+    sampleUSJs.forEach(function(value) {
+      it(`Validate ${value} and report error`, (inputUsjPath=value) => {
+        //Tests if input parses without errors
+        const testVaidator = new Validator()
+        assert(testVaidator instanceof Validator)
+        let inputUsj = fs.readFileSync(inputUsjPath, 'utf8')
+        inputUsj = inputUsj.replace("code", "cooode");
+        inputUsj = inputUsj.replace("content", "contents");
+        const usj = JSON.parse(inputUsj);
+        assert.ok(!testVaidator.isValidUSJ(usj));
+        assert(testVaidator.message !== "");
       });
     });
 });
--- web-usfm-parser/test/test_autofix.js
+++ web-usfm-parser/test/test_autofix.js
@@ -7,6 +7,7 @@ import {Validator} from '../src/index.js';
 
 const TEST_DIR = "../tests";
 const allUSFMFiles = glob.sync(TEST_DIR+'/autofix/*');
+const sampleUSJs = glob.sync(TEST_DIR+'/specExamples/*/origin.json')
 
 await Validator.init("./tree-sitter-usfm.wasm", "./tree-sitter.wasm");
 
@@ -22,6 +23,36 @@ describe("Try autofixing errors in USFM", () => {
         const secondTest = testVaidator.isValidUSFM(fixedUsfm);
         // assert.ok(!firstTest);
         assert.ok(secondTest);
+      });
+    });
+});
+
+
+describe("Validate USJ", () => {
+    sampleUSJs.forEach(function(value) {
+      it(`Validate ${value}`, (inputUsjPath=value) => {
+        //Tests if input parses without errors
+        const testVaidator = new Validator()
+        assert(testVaidator instanceof Validator)
+        const inputUsj = fs.readFileSync(inputUsjPath, 'utf8')
+        const usj = JSON.parse(inputUsj);
+        // assert.ok(!firstTest);
+        assert.ok(testVaidator.isValidUSJ(usj));
+      });
+    });
+
+
+    sampleUSJs.forEach(function(value) {
+      it(`Validate ${value} and report error`, (inputUsjPath=value) => {
+        //Tests if input parses without errors
+        const testVaidator = new Validator()
+        assert(testVaidator instanceof Validator)
+        let inputUsj = fs.readFileSync(inputUsjPath, 'utf8')
+        inputUsj = inputUsj.replace("code", "cooode");
+        inputUsj = inputUsj.replace("content", "contents");
+        const usj = JSON.parse(inputUsj);
+        // assert.ok(!firstTest);
+        assert.ok(!testVaidator.isValidUSJ(usj));
       });
     });
 });
