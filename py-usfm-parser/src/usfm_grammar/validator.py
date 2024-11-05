ea24e44cd403c1596a8b55bbd4d96a02816945cb
kavitharaju <kavitharaju18@gmail.com>
2024-11-05 11:35:50 +0530

    Handle pylint issues

--- py-usfm-parser/src/usfm_grammar/validator.py
+++ py-usfm-parser/src/usfm_grammar/validator.py
@@ -1,8 +1,8 @@
 '''Check the formats of USFM and USJ. Also tries to fixe common errors in USFM'''
 
+import re
 import json
 import jsonschema
-import re
 
 import tree_sitter_usfm3 as tsusfm
 from tree_sitter import Language, Parser
@@ -10,15 +10,17 @@ from tree_sitter import Language, Parser
 from usfm_grammar.usfm_parser import error_query
 
 class Validator:
+    '''Check validity of USJ and USFM. Also auto fix USFM'''
     def __init__(self, tree_sitter_usfm=tsusfm, usj_schema_path='../schemas/usj.js'):
-        USFM_LANGUAGE = Language(tree_sitter_usfm.language())
-        self.USFM_parser = Parser(USFM_LANGUAGE)
-        self.USFM_errors = []
+        '''contrsuctor'''
+        usfm_language = Language(tree_sitter_usfm.language())
+        self.usfm_parser = Parser(usfm_language)
+        self.usfm_errors = []
 
-        USJ_schema = None
+        usj_schema = None
         with open(usj_schema_path, 'r', encoding='utf-8') as json_file:
-            USJ_schema = json.load(json_file)
-        self.USJ_validator = jsonschema.validators.Draft7Validator(schema=USJ_schema)
+            usj_schema = json.load(json_file)
+        self.usj_validator = jsonschema.validators.Draft7Validator(schema=usj_schema)
 
         self.message = ""
         self.modified_usfm = ""
@@ -26,28 +28,30 @@ class Validator:
 
 
     def is_valid_usj(self, usj):
-        # validate(instance=usj, schema=self.USJ_schema)
+        '''Inputs: USJ, Output: True/false, details in message'''
+        # validate(instance=usj, schema=self.usj_schema)
         self.message = ""
         try:
-            self.USJ_validator.validate(usj)
-        except Exception as e:
-            self.message = str(e)
+            self.usj_validator.validate(usj)
+        except Exception as exce: #pylint: disable=W0703
+            self.message = str(exce)
             return False
         return True
 
     def is_valid_usfm(self, usfm):
+        '''Inputs: USFM, Output: True/false, details in message'''
         self.usfm_bytes = bytes(usfm, "utf8")
-        tree = self.USFM_parser.parse(self.usfm_bytes)
+        tree = self.usfm_parser.parse(self.usfm_bytes)
 
         # check for errors in the parse tree and raise them
         errors = error_query.captures(tree.root_node)
-        self.USFM_errors = []
+        self.usfm_errors = []
         self.message = ""
         if len(errors) > 0:
-            self.USFM_errors = [err[0] for err in errors]
+            self.usfm_errors = [err[0] for err in errors]
         self.check_for_missing(tree.root_node)
 
-        if len(self.USFM_errors) > 0:
+        if len(self.usfm_errors) > 0:
             self.message = self.format_errors()
             return False
         return True
@@ -56,13 +60,14 @@ class Validator:
         '''Identify and report the MISSING nodes also as errors'''
         for child in node.children:
             if child.is_missing :
-                self.USFM_errors.append(child)
+                self.usfm_errors.append(child)
             else:
                 self.check_for_missing(child)
 
     def format_errors(self):
+        '''Prettify error messages from parse tree, to print'''
         err_lines = []
-        for err in self.USFM_errors:
+        for err in self.usfm_errors:
             # print("---"*10)
             # print(err.is_error)
             # print(err.is_missing)
@@ -86,15 +91,19 @@ class Validator:
 
 
 
-    def auto_fix_usfm(self, usfm, fixed=False):
+    def auto_fix_usfm(self, usfm, fixed=False): #pylint: disable=R0912,R0915
+        '''Iteratively tries to fix the errors found in usfm
+        input: usfm string
+        output: fixed usfm
+        details in message'''
         if self.is_valid_usfm(usfm):
             self.message = "Fixed Errors in USFM" if fixed else "No Errors in USFM"
             return usfm
         self.modified_usfm = usfm
         changed = False
-        for error in self.USFM_errors:
+        for error in self.usfm_errors:
             error_text = error.text.decode('utf-8')
-            # No \P after \s5 
+            # No \P after \s5
             if error.is_error and \
                error_text.startswith("\\s5") and \
                "paragraph" not in [ch.type for ch in error.children]:
@@ -167,7 +176,7 @@ class Validator:
                 # print(to_replace)
                 self.modified_usfm = self.modified_usfm.replace(to_replace, repalcement)
                 changed=True
-            # empty attribute   
+            # empty attribute
             elif error_text.strip() == "|":
                 # print("Match 10")
                 start = max(0, error.start_byte-5)
@@ -183,8 +192,8 @@ class Validator:
                 # print("Match 10")
                 self.modified_usfm = self.modified_usfm.replace(error_text, "")
                 changed=True
-                 
-            
+
+
         # print(f"{changed=}")
         if not changed:
             err_str = self.format_errors()
@@ -198,4 +207,17 @@ book_code_missing_pattern = re.compile(r'\\id[\s\n\r]*\\')
 v_without_space_pattern = re.compile(r'(\\v)(\d+)')
 c_without_space_pattern = re.compile(r'(\\c)(\d+)')
 
-valid_markers_pattern = re.compile(r'(\\id|\\usfm|\\ide|\\ref|\\h|\\toc|\\toca|\\sts|\\rem|\\restore|\\lit|\\iqt|\\iqt|\\imt|\\imte|\\is|\\io|\\ior|\\ior|\\iot|\\ip|\\im|\\ipi|\\imi|\\ili|\\ipq|\\imq|\\ipr|\\ib|\\iq|\\ie|\\iex|\\v|\\va|\\va|\\vp|\\vp|\\c|\\cl|\\ca|\\ca|\\cp|\\cd|\\mt|\\mte|\\ms|\\mr|\\s|\\sr|\\r|\\sp|\\d|\\sd|\\p|\\m|\\po|\\pr|\\cls|\\pmo|\\pm|\\pmc|\\pmr|\\pi|\\mi|\\nb|\\pc|\\ph|\\phi|\\b|\\q|\\qr|\\qc|\\qs|\\qs|\\qa|\\qac|\\qac|\\qm|\\qd|\\lh|\\lf|\\li|\\lim|\\liv|\\lik|\\lik|\\litl|\\litl|\\tr|\\th|\\thr|\\tc|\\tcr|\\f|\\f|\\fe|\\fe|\\ef|\\ef|\\fr|\\fr|\\fq|\\fq|\\fqa|\\fqa|\\fk|\\fk|\\fl|\\fl|\\fw|\\fw|\\fp|\\fp|\\ft|\\ft|\\fdc|\\fdc|\\fv|\\fv|\\fm|\\fm|\\x|\\x|\\xo|\\xo|\\xk|\\xk|\\xq|\\xq|\\xt|\\xt|\\xt|\\xt|\\xta|\\xta|\\xop|\\xop|\\xot|\\xot|\\xnt|\\xnt|\\xdc|\\xdc|\\rq|\\rq|\\add|\\add|\\bk|\\bk|\\dc|\\dc|\\k|\\k|\\nd|\\nd|\\ord|\\ord|\\pn|\\pn|\\png|\\png|\\addpn|\\addpn|\\qt|\\qt|\\sig|\\sig|\\sls|\\sls|\\tl|\\tl|\\wj|\\wj|\\em|\\em|\\bd|\\bd|\\it|\\it|\\bdit|\\bdit|\\no|\\no|\\sc|\\sc|\\sup|\\sup|\\ndx|\\ndx|\\pro|\\pro|\\rb|\\rb|\\w|\\w|\\wg|\\wg|\\wh|\\wh|\\wa|\\wa|\\fig|\\jmp|\\jmp|\\pb|\\z|\\z|\\esb|\\esbe|\\cat|\\cat)(\d|\s|\n|\r|$)')
+valid_markers_pattern = re.compile(r'(\\id|\\usfm|\\ide|\\ref|\\h|\\toc|\\toca|\\sts|\\rem|'+\
+    r'\\restore|\\lit|\\iqt|\\iqt|\\imt|\\imte|\\is|\\io|\\ior|\\ior|\\iot|\\ip|\\im|\\ipi|'+\
+    r'\\imi|\\ili|\\ipq|\\imq|\\ipr|\\ib|\\iq|\\ie|\\iex|\\v|\\va|\\va|\\vp|\\vp|\\c|\\cl|'+\
+    r'\\ca|\\ca|\\cp|\\cd|\\mt|\\mte|\\ms|\\mr|\\s|\\sr|\\r|\\sp|\\d|\\sd|\\p|\\m|\\po|\\pr|'+\
+    r'\\cls|\\pmo|\\pm|\\pmc|\\pmr|\\pi|\\mi|\\nb|\\pc|\\ph|\\phi|\\b|\\q|\\qr|\\qc|\\qs|'+\
+    r'\\qs|\\qa|\\qac|\\qac|\\qm|\\qd|\\lh|\\lf|\\li|\\lim|\\liv|\\lik|\\lik|\\litl|\\litl|'+\
+    r'\\tr|\\th|\\thr|\\tc|\\tcr|\\f|\\f|\\fe|\\fe|\\ef|\\ef|\\fr|\\fr|\\fq|\\fq|\\fqa|\\fqa|'+\
+    r'\\fk|\\fk|\\fl|\\fl|\\fw|\\fw|\\fp|\\fp|\\ft|\\ft|\\fdc|\\fdc|\\fv|\\fv|\\fm|\\fm|\\x|'+\
+    r'\\x|\\xo|\\xo|\\xk|\\xk|\\xq|\\xq|\\xt|\\xt|\\xt|\\xt|\\xta|\\xta|\\xop|\\xop|\\xot|'+\
+    r'\\xot|\\xnt|\\xnt|\\xdc|\\xdc|\\rq|\\rq|\\add|\\add|\\bk|\\bk|\\dc|\\dc|\\k|\\k|\\nd|'+\
+    r'\\nd|\\ord|\\ord|\\pn|\\pn|\\png|\\png|\\addpn|\\addpn|\\qt|\\qt|\\sig|\\sig|\\sls|'+\
+    r'\\sls|\\tl|\\tl|\\wj|\\wj|\\em|\\em|\\bd|\\bd|\\it|\\it|\\bdit|\\bdit|\\no|\\no|\\sc|'+\
+    r'\\sc|\\sup|\\sup|\\ndx|\\ndx|\\pro|\\pro|\\rb|\\rb|\\w|\\w|\\wg|\\wg|\\wh|\\wh|\\wa|'+\
+    r'\\wa|\\fig|\\jmp|\\jmp|\\pb|\\z|\\z|\\esb|\\esbe|\\cat|\\cat)(\d|\s|\n|\r|$)')
