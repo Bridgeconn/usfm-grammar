f52caf78ddb5f9a244bb7dc25f9b51a5cbe6e21e
amal-J-k <22l024@gecskp.ac.in>
2025-01-13 20:52:44 +0530

    rust floder init

---
+++ rust-usfm-parser/target/debug/deps/rust_usfm_parser-8e0e33a228fa19e5.d
@@ -0,0 +1,5 @@
+/home/unix/Documents/internship/usfm-grammar/rust-usfm-parser/target/debug/deps/librust_usfm_parser-8e0e33a228fa19e5.rmeta: src/main.rs
+
+/home/unix/Documents/internship/usfm-grammar/rust-usfm-parser/target/debug/deps/rust_usfm_parser-8e0e33a228fa19e5.d: src/main.rs
+
+src/main.rs:
