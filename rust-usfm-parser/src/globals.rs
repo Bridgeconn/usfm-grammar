c1cee913d6346bf4b3722c91de60fd3ce8705445
amal-J-k <143809111+amal-J-k@users.noreply.github.com>
2025-02-03 15:13:18 +0530

    Update globals.rs

--- rust-usfm-parser/src/globals.rs
+++ rust-usfm-parser/src/globals.rs
@@ -1,6 +1,6 @@
+use lazy_static::lazy_static;
 use std::sync::Mutex;
-use tree_sitter::Tree;
-use lazy_static::lazy_static; // Import lazy_static directly
+use tree_sitter::Tree; // Import lazy_static directly
 
 lazy_static! {
     pub static ref GLOBAL_TREE: Mutex<Option<Tree>> = Mutex::new(None);
