7fc11de5df848d15fc5a2560b0528750bb54d59e
kavitharaju <kavitharaju18@gmail.com>
2025-03-10 12:20:31 +0530

    Include BibleNLP's default vref in code base

--- py-usfm-parser/src/usfm_grammar/__init__.py
+++ py-usfm-parser/src/usfm_grammar/__init__.py
@@ -2,11 +2,13 @@
 
 from usfm_grammar import usfm_parser
 from usfm_grammar import validator
+from usfm_grammar import vrefs
 
 Filter = usfm_parser.Filter
 Format = usfm_parser.Format
 USFMParser = usfm_parser.USFMParser
 
 Validator = validator.Validator
+original_vref = vrefs.original_vref
 
 __version__ = "3.0.1-alpha.2"
