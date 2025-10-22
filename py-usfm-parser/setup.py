9bc253c276670d06f3965c96f1e4b7ffbb48f426
Kavitha Raju <kavitharaju18@gmail.com>
2025-10-22 10:51:03 +0530

    Bump version: 3.1.3 → 3.2.0-beta.1

--- py-usfm-parser/setup.py
+++ py-usfm-parser/setup.py
@@ -9,7 +9,7 @@ class BinaryDistribution(Distribution):
 
 setup(
     name="usfm-grammar",  # Required
-    version="3.1.3",  # Required
+    version="3.2.0-beta.1",  # Required
     python_requires=">=3.10",
     # install_requires=["tree-sitter==0.22.3",
     # "tree-sitter-usfm3==3.0.0-beta.7",
